564f3de54ed4299b2483b45df371b5670f1a57c5
Thiago_Batista_da_Silva_Oliveira <gmoliveira69@gmail.com>
2024-03-16 13:59:43 -0300

    feat: user info

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import { Container, Greeting, Message, Name } from './styles';
+import {Power} from 'phosphor-react-native';
+import  {useUser, useApp } from '@realm/react';
+import { Container, Greeting, Message, Name, Picture } from './styles';
+import { TouchableOpacity } from 'react-native';
+import theme from '../../theme';
 
 export function HomeHeader() {
+  const user = useUser();
+  const app = useApp();
+
+  function handleLogout() {
+    app.currentUser?.logOut();
+  }
   return (
     <Container>
+      <Picture source={{
+        uri: user?.profile.pictureUrl
+      }} 
+       placeholder = 'L184i9kCbIof00ayjZay~qj[ayj@'
+      />
         <Greeting>
             <Message>Olá</Message>
-            <Name>Thiago</Name>
+            <Name>{user?.profile.name}</Name>
         </Greeting>
+        <TouchableOpacity activeOpacity={0.7} onPress={handleLogout}>
+          <Power size={32} color={theme.COLORS.GRAY_400} />
+        </TouchableOpacity>
     </Container>
   );
 }
