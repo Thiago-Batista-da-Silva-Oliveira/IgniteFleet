1681122f45ca48c948ae5f4513f6a613936f005d
Thiago_Batista_da_Silva_Oliveira <gmoliveira69@gmail.com>
2024-03-19 23:20:42 -0300

    feat: departure screen

---
+++ src/components/LicensePlateInput/index.tsx
@@ -0,0 +1,24 @@
+import { useTheme } from 'styled-components/native';
+import { TextInputProps } from 'react-native';
+import { Container, Input, Label } from './styles';
+
+type Props = TextInputProps & {
+    label: string;
+}
+
+export function LicensePlateInput({ label, ...rest }: Props) {
+    const { COLORS } = useTheme();
+  return (
+    <Container>
+     <Label>
+       {label}
+     </Label>
+     <Input
+       maxLength={7}
+       autoCapitalize='characters'
+       placeholderTextColor={COLORS.GRAY_400}
+       {...rest}
+    />
+    </Container>
+  );
+}
--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,10 +1,18 @@
-import { Header } from '../../components/Header';
-import { Container } from './styles';
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { LicensePlateInput } from "../../components/LicensePlateInput";
+import { TextAreaInput } from "../../components/TextAreaInput";
+import { Container, Content } from "./styles";
 
 export function Departure() {
   return (
     <Container>
-      <Header title='Saída'/>
+      <Header title="Saída" />
+      <Content>
+        <LicensePlateInput label="Placa do veículo" placeholder="BRA1234" />
+        <TextAreaInput placeholder="Vou utilizar o veículo para..." label="Finalidade" />
+      <Button title="Registrar Saída" />
+     </Content>
     </Container>
   );
 }
